a06edd7faddcb4789ab1e9a6a6f5fc5d4a8f3875
Jason Pearson <jason.d.pearson@gmail.com>
2013-04-22 17:30:26 -0400

    removed sedis dependency

--- project/Build.scala
+++ project/Build.scala
@@ -4,12 +4,10 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-	val appName         = "tin-roof"
+	val appName         = "play-angular-require-example"
 	val appVersion      = "1.0-SNAPSHOT"
 
-	val appDependencies = Seq(
-		"org.sedis" % "sedis_2.9.2" % "1.1.0"
-	)
-	
+	val appDependencies = Seq()
+
 	val main = play.Project(appName,appVersion,appDependencies)
 }
